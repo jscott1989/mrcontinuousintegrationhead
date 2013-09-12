c54166c011791875c0a155fd5c86548da1b5d73f
Jonathan Scott <jonathanscott1989@gmail.com>
2013-09-12 13:28:40 +0100

    Test travis setup

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+	MrContinuousIntegrationHead
+'''
+
+
+class MrContinuousIntegrationHead(object):
+	def main(self):
+		print "DONE"
+
+if __name__ == "__main__":
+	MrContinuousIntegrationHead().main()
