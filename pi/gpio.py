09ba034e71a137c6ee1381f35075c31ceae39e22
Jonathan Scott <jonathanscott1989@gmail.com>
2013-09-16 13:20:11 +0100

    Only work on PI

--- pi/gpio.py
+++ pi/gpio.py
@@ -1,34 +1,34 @@
-try:
-	from RPi import GPIO
+# try:
+from RPi import GPIO
 
-	from GPIO import IN, OUT, BOARD, BCM, HIGH, LOW, output, cleanup
+from GPIO import IN, OUT, BOARD, BCM, HIGH, LOW, output, cleanup
 
-	def map(mode, channels):
-		GPIO.setmode(mode)
-		for channel, value in channels.items():
-			GPIO.setup(channel, value)
-except ImportError:
-	# No RPi - probably not running on a PI - simulate this
-	IN = "in"
-	OUT = "out"
-	BOARD = "board"
-	BCM = "bcm"
-	HIGH = 1
-	LOW = 0
+def map(mode, channels):
+	GPIO.setmode(mode)
+	for channel, value in channels.items():
+		GPIO.setup(channel, value)
+# except ImportError:
+# 	# No RPi - probably not running on a PI - simulate this
+# 	IN = "in"
+# 	OUT = "out"
+# 	BOARD = "board"
+# 	BCM = "bcm"
+# 	HIGH = 1
+# 	LOW = 0
 
-	_mode = None
-	_channels = [{"mode": None, "value": LOW} for x in range(20)]
+# 	_mode = None
+# 	_channels = [{"mode": None, "value": LOW} for x in range(20)]
 
-	def map(mode, channels):
-		_mode = mode
-		for channel, value in channels.items():
-			_channels[channel]["mode"] = value
+# 	def map(mode, channels):
+# 		_mode = mode
+# 		for channel, value in channels.items():
+# 			_channels[channel]["mode"] = value
 
-	def output(channel, value):
-		_channels[channel]["value"] = value
+# 	def output(channel, value):
+# 		_channels[channel]["value"] = value
 
-	def cleanup():
-		pass
+# 	def cleanup():
+# 		pass
 
 def _cleanup():
 	''' Release pins on exit '''
