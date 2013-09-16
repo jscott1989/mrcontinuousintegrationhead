586d6cf4146f52b901206c5b2e1112ed6e786cfd
Jonathan Scott <jonathanscott1989@gmail.com>
2013-09-16 14:13:55 +0100

    ahhh

--- pi/servo.py
+++ pi/servo.py
@@ -1,4 +1,4 @@
 import subprocess
 
 def set_position(n, position):
-	subprocess.call("sudo echo %d=%d > /dev/servoblaster" % (n, position), shell=True)
+	subprocess.call("sudo echo %s=%s > /dev/servoblaster" % (n, position), shell=True)
